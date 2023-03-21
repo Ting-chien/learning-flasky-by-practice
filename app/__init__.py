e8e7eecb8d5a0b006478901f09fcf9d2229635c4
Eric Wang <eric.wang@theomnieyes.com>
2023-03-21 23:30:38 +0800

    Add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
 def create_app(config):
@@ -6,8 +11,11 @@ def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
 
+    bootstrap.init_app(app)
+    db.init_app(app)
+
     @app.route('/')
     def index():
-        return f'SECRET_KEY={app.config.get("SECRET_KEY")}'
+        return "Hello Flask!"
     
     return app
